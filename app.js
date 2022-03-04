54f20c7dc460395467d82ee5a3d253644cfb1eca
AngelaC123 <angelarare@gmail.com>
2022-03-04 21:25:28 +0800

    Add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const port = 3000
 
@@ -16,8 +17,11 @@ db.once('open', () => {
   console.log('mongdodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send(`This is a restaurant list update!`)
+  res.render('index')
 })
 
 app.listen(port, () => {
