e15e98bf826bf8dd7e07b6b52a7bb0497d24386d
AngelaC123 <angelarare@gmail.com>
2022-03-04 20:18:55 +0800

    Add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list-crud')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongdodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send(`This is a restaurant list update!`)
