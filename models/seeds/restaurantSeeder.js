b8f79aeea666ee9396c1f4b3359ca4d667591fc7
AngelaC123 <angelarare@gmail.com>
2022-03-04 21:15:26 +0800

    Add restaurant by importing restaurant.json

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json')
+console.log(restaurantList)
+
+mongoose.connect('mongodb://localhost/restaurant-list-crud')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+
+  restaurantList.results.forEach((data) => Restaurant.create({
+    name: data.name,
+    category: data.category,
+    image: data.image,
+    location: data.location,
+    phone: data.phone,
+    google_map: data.google_map,
+    rating: data.rating,
+    description: data.description
+  }))
+  console.log('done')
+})
