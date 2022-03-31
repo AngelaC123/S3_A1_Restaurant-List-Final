5196880f31dfb53622d6d389b4303640619bdc8e
AngelaC123 <angelarare@gmail.com>
2022-03-31 10:48:17 +0800

    Add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverRide = require('method-override')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
@@ -12,10 +13,15 @@ const Restaurant = require('./models/restaurant')
 require('./config/mongoose.js')
 
 
+app.use(express.static('public'))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.static('public'))
+app.use(session({
+  secret: 'RestaurantListSecret',
+  resave: false,
+  saveUninitialized: true,
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverRide('_method'))
 app.use(routes)
