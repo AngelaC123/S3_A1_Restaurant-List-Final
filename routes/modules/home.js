e99d648522fb47057f5c8f67bae0ebeaa1184d04
AngelaC123 <angelarare@gmail.com>
2022-04-01 18:16:50 +0800

    Modify search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -20,7 +20,17 @@ router.get('/', (req, res) => {
     const keywords = req.query.keywords
     const keyword = req.query.keywords.trim().toLowerCase()
 
-    return Restaurant.find({ $or: [{ name: { $regex: keyword, $options: 'i' } }, { category: { $regex: keyword, $options: 'i' } }] })
+    return Restaurant.find({
+      $and: [
+        { userId },
+        {
+          $or: [
+            { name: { $regex: keyword, $options: 'i' } },
+            { category: { $regex: keyword, $options: 'i' } }
+          ]
+        }
+      ]
+    })
       .lean()
       .sort(CurrentMode(sort))
       .then(restaurant => res.render('index', { restaurant, keyword: keywords, sort }))
@@ -28,7 +38,6 @@ router.get('/', (req, res) => {
 
       .catch(error => {
         console.log(error)
-        // res.render('errorPage', { status: 500, error: error.message })
       })
   }
 
